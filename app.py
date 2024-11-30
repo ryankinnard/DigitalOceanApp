0990182cc961a2567834c070923d8d6ccc959fb5
Ryan Kinnard <ryankinnard@Ryans-MacBook-Air.local>
2024-11-30 17:16:00 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+pymysql.install_as_MySQLdb()
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+with app.app_context():
+    db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+@app.route('/')
+def index():
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
+
+@app.route("/add", methods=["POST"])
+def add():
+    # add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/complete/<int:todo_id>")
+def complete(todo_id):
+    with db.session() as session:  # Use a session context
+        todo = session.get(Todo, todo_id)  # Use Session.get() instead of Query.get()
+        if todo:  # Check if the record exists
+            todo.complete = not todo.complete
+            session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    with db.session() as session:  # Use a session context
+        todo = session.get(Todo, todo_id)  # Use Session.get() instead of Query.get()
+        db.session.delete(todo)
+        db.session.commit()
+    return redirect(url_for("index"))
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)  
+
